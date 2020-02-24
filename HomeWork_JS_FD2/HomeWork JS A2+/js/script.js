9c67a95f1304298a0e43d6f41f88fb04f497a330
Dreyka <32411577+Dreyka@users.noreply.github.com>
2020-02-24 21:25:48 +0300

    Add files via upload

---
+++ HomeWork_JS_FD2/HomeWork JS A2+/js/script.js
@@ -0,0 +1,22 @@
+(function () {
+    function del(valueStr) {
+        let countPre = 0;
+        for (let i = 0; valueStr[i] === " "; i++) {
+            countPre++;
+        }
+        let countPost = 0;
+        for (let i = valueStr.length - 1; valueStr[i] === " "; i--) {
+            countPost++;
+        }
+        if(countPre===valueStr.length){
+            return valueStr="";
+        } else if(countPre===0 && countPost===0) {
+            return valueStr;
+        } else {
+            return valueStr = valueStr.substr(countPre, valueStr.length - (countPost + countPre));
+        }
+    };
+    let word = prompt("Введите слова");
+    console.log("*" + del(word) + "*");
+    alert("*" + del(word) + "*");
+})();
