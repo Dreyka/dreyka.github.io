93a78e03971ea1beb7d093f7bb1fb991e3e8864c
Dreyka <32411577+Dreyka@users.noreply.github.com>
2020-02-28 11:25:24 +0300

    Add files via upload

---
+++ HomeWork_JS_FD2/HomeWork JS A3+/js/script.js
@@ -0,0 +1,27 @@
+(function () {
+        function pal(valueStr) {
+            valueStr = valueStr.toLowerCase().trim();
+            let symbol = {" ": 1, ",": 1, ".": 1, ":": 1, ";": 1, "ь": 1, "ъ": 1, "!": 1, "?": 1};
+            let clearWord = "";
+            for (let i = 0; i < valueStr.length; i++) {
+                let v = valueStr[i];
+                if (v === "ё") {
+                    clearWord = clearWord + "е";
+                } else if (!(v in symbol)) {
+                    clearWord = clearWord + v;
+                }
+                ;
+            }
+            ;
+            for (let i = 0; i < clearWord.length / 2; i++) {
+                if (clearWord[i] !== clearWord[clearWord.length - (i + 1)]) return false;
+            }
+            ;
+            return true;
+        }
+        ;
+        let word = prompt("Введите слово");
+        console.log("*" + pal(word) + "*");
+    }
+
+)();
