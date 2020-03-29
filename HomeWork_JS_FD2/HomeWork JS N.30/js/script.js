0c60f08a568459358f5c095aee5cada7f287784e
Dreyka <32411577+Dreyka@users.noreply.github.com>
2020-03-29 20:52:45 +0300

    Add files via upload

---
+++ HomeWork_JS_FD2/HomeWork JS N.30/js/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+function findElem(EO) {
+    EO = EO || window.event;
+    let area = document.getElementById("Area");
+    let img = area.getElementsByTagName("img");
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.left = img[i].offsetLeft + "px";
+        img[i].style.top = img[i].offsetTop + "px";
+    }
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.position = "absolute";
+    }
+}
+window.addEventListener("DOMContentLoaded", findElem, false);
+
+function elemStart(EO) {
+    EO = EO || window.event;
+    let posX = EO.clientX - this.offsetLeft;
+    let posY = EO.clientY - this.offsetTop;
+    for (let i = 0; i < img.length; i++) {
+        img[i].style.zIndex = "1";
+    }
+    ;
+    this.style.zIndex = "3";
+    this.style.cursor = "progress";
+
+    function elemMove(EO) {
+        EO = EO || window.event;
+        this.style.left = (EO.clientX - posX) + "px";
+        this.style.top = (EO.clientY - posY) + "px";
+        this.style.zIndex = "3";
+    };
+
+    function elemEnd(EO) {
+        EO = EO || window.event;
+        this.removeEventListener("mousemove", elemMove, false);
+        this.style.cursor = "auto";
+    }
+
+    this.addEventListener("mousemove", elemMove, false);
+    this.addEventListener("mouseup", elemEnd, false);
+}
+
+let area = document.getElementById("Area");
+let img = area.getElementsByTagName("img");
+
+
+for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener("mousedown", elemStart, false);
+    img[i].ondragstart = function () {
+        return false;
+    };
+}
+
+
